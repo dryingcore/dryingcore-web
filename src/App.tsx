76634ffe3cb9ddc04cc04153d6448240059abeda
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-13 17:54:03 +0000

    Jul 13, 2024, 2:54 PM

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./components/Home"
 function App() {
 
   return (
-    <Box sx={{ backgroundImage: "url(../public/)", backgroundSize: "cover", width: "100vw", height: "100vh" }}>
+    <Box sx={{ backgroundImage: "url(../public/retrowave_wallpaper__1920_x_1080__by_thejesseplug_dbk08o8-pre.jpg)", backgroundSize: "", width: "100vw", height: "100vh" }}>
       <Home />
     </Box>
   )
