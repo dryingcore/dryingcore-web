7d3a839b22a046e338958f23f118260213a8a2a9
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-13 17:19:53 +0000

    Jul 13, 2024, 2:19 PM

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Box, Container } from "@mui/material"
 import BasicMenu from "./components/Menu"
 
 function App() {
 
   return (
-    <>
+    <Box sx={{ backgroundColor: "darkgreen", height: "100vh" }}>
       <BasicMenu />
-    </>
+    </Box>
   )
 }
 
