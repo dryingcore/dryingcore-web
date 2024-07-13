5f2720f67c815225ce488ddb5b63fb838ea00791
dryingcore <protonmail.default.sense07@protonmail.com>
2024-07-13 17:36:39 +0000

    Jul 13, 2024, 2:36 PM

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,8 +3,14 @@ import BasicMenu from "./Menu"
 
 export default function Home() {
     return (
-        <Box sx={{ backgroundColor: "cadetblue", display: "flex" }}>
-            <BasicMenu />
-        </Box>
+        <>
+            <Box sx={{ backgroundColor: "cadetblue", display: "flex" }}>
+                <BasicMenu />
+            </Box>
+
+            <Box>
+                <h1>Home</h1>
+            </Box>
+        </>
     )
 }
